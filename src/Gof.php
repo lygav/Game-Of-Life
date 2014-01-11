070510f31913402c1104189665dd2eea919ecfaf
Vlad <lyvladi@gmail.com>
2014-01-11 17:35:38 +0200

    Refactor

--- src/Gof.php
+++ src/Gof.php
@@ -1,73 +1,74 @@
 <?php
 
-class Gof {
+class Gof
+{
 
-    function __construct(array $live_cells)
-    {
-        $this->live_cells = $live_cells;
-    }
+	public function __construct (array $live_cells)
+	{
+		$this->live_cells = $live_cells;
+	}
 
-    function evolve()
-    {
-        if (count($this->live_cells) <= 1) {
-            return array();
-        } else {
-            return $this->next_generation();
-        }
-    }
+	public function evolve ()
+	{
+		if (count($this->live_cells) <= 1) {
+			return array();
+		} else {
+			return $this->next_generation();
+		}
+	}
 
-    private function next_generation()
-    {
-        $next_gen = array();
-        $cell_reference_frequencies = $this->calculate_neigbours_frequencies();
-        foreach ($cell_reference_frequencies as $cell => $frequency) {
-            if ($this->should_cell_live($cell, $frequency)) {
-	            $next_gen[] = $cell;
-            }
-        }
-        return $this->live_cells = $next_gen;
-    }
+	private function next_generation ()
+	{
+		$next_gen = array();
+		$cell_ref_frequencies = $this->calcualte_cell_ref_frequencies();
+		foreach ($cell_ref_frequencies as $cell => $frequency) {
+			if ($this->should_cell_live($cell, $frequency)) {
+				$next_gen[] = $cell;
+			}
+		}
+		return $this->live_cells = $next_gen;
+	}
 
-    private function should_cell_live($cell, $frequency)
-    {
-        if (false !== array_search($cell, $this->live_cells)) {
-	        return $this->should_live_cell_live($frequency);
-        } else {
-	        return $this->should_dead_cell_live($frequency);
-        }
-    }
+	private function should_cell_live ($cell, $frequency)
+	{
+		if (FALSE !== array_search($cell, $this->live_cells)) {
+			return $this->should_stay_alive($frequency);
+		} else {
+			return $this->should_become_alive($frequency);
+		}
+	}
 
-    private function should_live_cell_live($frequency)
-    {
-        return ($frequency == 2 or $frequency == 3);
-    }
+	private function should_stay_alive ($frequency)
+	{
+		return ($frequency == 2 OR $frequency == 3);
+	}
 
-    private function should_dead_cell_live($frequency)
-    {
-        return $frequency == 3;
-    }
+	private function should_become_alive ($frequency)
+	{
+		return $frequency == 3;
+	}
 
-    private function calculate_neigbours_frequencies()
-    {
-        $total_frequencies = array();
-        foreach ($this->live_cells as $cell) {
-            list($x, $y) = explode(',', $cell);
-            $total_frequencies = array_merge($total_frequencies, $this->get_neighbours(array('x' => $x, 'y' => $y)));
-        }
-        return array_count_values($total_frequencies);
-    }
+	private function calcualte_cell_ref_frequencies ()
+	{
+		$total_frequencies = array();
+		foreach ($this->live_cells as $cell) {
+			list($x, $y) = explode(',', $cell);
+			$total_frequencies = array_merge($total_frequencies, $this->get_cell_neighbours(array('x' => $x, 'y' => $y)));
+		}
+		return array_count_values($total_frequencies);
+	}
 
-    private function get_neighbours(array $cell)
-    {
-        $neighbours_coordinates_range = range(-1, 1);
-        $frequencies = array();
-        foreach ($neighbours_coordinates_range as $x) {
-            foreach ($neighbours_coordinates_range as $y) {
-                if (($x.','.$y) !== '0,0') {
-	                $frequencies[] = ($cell['x'] + $x) . ',' . ($cell['y'] + $y);
-                }
-            }
-        }
-        return $frequencies;
-    }
+	private function get_cell_neighbours (array $cell)
+	{
+		$offsets = array(- 1, 0, 1);
+		$neighbours = array();
+		foreach ($offsets as $offset_x) {
+			foreach ($offsets as $offset_y) {
+				if (($offset_x . ',' . $offset_y) !== '0,0') {
+					$neighbours[] = ($cell['x'] + $offset_x) . ',' . ($cell['y'] + $offset_y);
+				}
+			}
+		}
+		return $neighbours;
+	}
 }
--- tests/gofTest.php
+++ tests/gofTest.php
@@ -5,38 +5,38 @@ include '../autoloader.php';
 class GofTest extends PHPUnit_Framework_TestCase
 {
 
-    function testLiveCellWithNoNeigboursDies()
-    {
-        $game = new Gof(array('1,1'));
-        $this->assertEmpty($game->evolve());
-    }
+	public function testLiveCellWithNoNeigboursDies ()
+	{
+		$game = new Gof(array('1,1'));
+		$this->assertEmpty($game->evolve());
+	}
 
-    function testLiveCellWithOneNeighbourDies()
-    {
-        $game = new Gof(array('0,0', '1,1'));
-        $this->assertEmpty($game->evolve());
-    }
+	public function testLiveCellWithOneLiveNeighbourDies ()
+	{
+		$game = new Gof(array('0,0', '1,1'));
+		$this->assertEmpty($game->evolve());
+	}
 
-    function testLiveCellWithTwoNeigboursLive()
-    {
-        $game = new Gof(array('10,10', '11,11', '9,9'));
-        $next_gen = $game->evolve();
-        $this->assertContains('10,10', $next_gen);
-    }
+	public function testLiveCellWithTwoLiveNeigboursLives ()
+	{
+		$game     = new Gof(array('10,10', '11,11', '9,9'));
+		$next_gen = $game->evolve();
+		$this->assertContains('10,10', $next_gen);
+	}
 
-    function testLiveCellWithThreeNeigboursLive()
-    {
-        $game = new Gof(array('10,10', '11,11', '9,9', '11,9'));
-        $next_gen = $game->evolve();
-        $this->assertContains('10,10', $next_gen);
-    }
+	public function testLiveCellWithThreeLiveNeigboursLives ()
+	{
+		$game     = new Gof(array('10,10', '11,11', '9,9', '11,9'));
+		$next_gen = $game->evolve();
+		$this->assertContains('10,10', $next_gen);
+	}
 
-    function testDeadCellWithThreeNeighboursBecomesAlive()
-    {
-        $game = new Gof(array('0,0', '2,0', '2,2'));
-        $next_gen = $game->evolve();
-        $this->assertTrue(count($next_gen) == 1);
-        $this->assertContains('1,1', $next_gen);
-    }
+	public function testDeadCellWithThreeLiveNeighboursBecomesAlive ()
+	{
+		$game     = new Gof(array('0,0', '2,0', '2,2'));
+		$next_gen = $game->evolve();
+		$this->assertTrue(count($next_gen) == 1);
+		$this->assertContains('1,1', $next_gen);
+	}
 
 }
